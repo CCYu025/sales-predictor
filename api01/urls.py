43d0021b971a06588c11750f3dff36c8e5028a2d
Yu <kenjo0530@yahoo.com.tw>
2025-04-28 16:09:04 +0800

    fix: add index view for home page

--- api01/urls.py
+++ api01/urls.py
@@ -1,28 +1,9 @@
 # 余振中 (Yu Chen Chung)
-from rest_framework import routers
-from django.urls import  path, include
-from api01 import views
 
+from django.urls import path
 from .views import PredictView, index
 
 urlpatterns = [
-    path('', index, name='index'),               # 網站首頁 ➔ 顯示網頁
-    path('predict/', PredictView.as_view(), name='predict'),  # API 路徑
+    path('', index, name='index'),                # 這行要有，才能顯示首頁！
+    path('predict/', PredictView.as_view(), name='predict'),
 ]
-
-# 建立 路由器
-# router = routers.DefaultRouter()
-# 註冊 網址 對硬 視圖
-# 127.0.0.1:8000/api 由 myproject/urls.py 負責設定
-# 127.0.0.1:8000/api/products
-# router.register(r'products', views.ProductViewSet)
-
-# /api/product
-# api 第一層路徑
-# 建立列表 路徑 對映 網址 , path(路徑 , 網址)
-# 不能打錯字 urlpatterns
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('user/<str:username>/orders/', UserOrderListView.as_view(),name='user_orders'),
-# ]
-
