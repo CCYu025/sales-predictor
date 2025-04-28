979dd0e529ed1a95d11f6ce79dfa8701b9b602da
Yu <kenjo0530@yahoo.com.tw>
2025-04-28 16:45:24 +0800

    feat: add root redirect to /api/

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # ⭐⭐ 加這行
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api01.urls')),
+    path('', lambda request: redirect('api/', permanent=False)),  # ⭐⭐ 加這行！根目錄自動跳轉
 ]
